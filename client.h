8bba313a3a9e66cc901842a9460ff1adf77620b6
leehahoon <leehahoon1001@gmail.com>
2018-11-15 17:53:16 +0900

    Update client.h

--- client.h
+++ client.h
@@ -1,11 +1,37 @@
 #include "common.h"
 
-struct CLIENT {
-	struct CLIENT *next;
+typedef struct Client {
+	struct Client * next;
+	int stdNum;
 	char *pw;
 	char *name;
 	char *address;
 	char *dial;
-	int stuNum;
-};
+} Client;
 
+void strAllocate(char **chr, char *buf);
+//구조체 멤버 변수 중, char 포인터형 변수 할당의 편의를 위한 함수
+
+
+void register_client(Client *a);
+//Client 회원가입
+//학번, 비밀번호 등 입력받고 Client *a에 저장
+
+
+void addClient(Client *head, Client *cli);
+//Client 구조체를 관리함에 있어서 링크드리스트 노드를 추가하기 위한 함수
+//Client *head : 관리하는 링크드리스트의 head 노드
+//Client *cli : 가장 맨 뒤에 추가하려는 Client 구조체
+/*
+Remove, Modify, Sorting 등 추가 예정
+*/
+
+
+void PrintClientInfo(Client *cli);
+//Client 구조체의 정보 출력
+
+void PrintNodes(Client *cli);
+//Client 링크드리스트 노드 정보 출력
+
+void File2Node(Client *head);
+//Client 파일을 읽어 링크드리스트 노드로 변환
