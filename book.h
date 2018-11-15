58432ff238d752ed405700ba91b2e36838b1f8e2
Wookje Kwon <wookje.happy@gmail.com>
2018-11-15 15:21:45 +0900

    functions added

--- book.h
+++ book.h
@@ -1,13 +1,45 @@
 #include "common.h"
 
-struct book {
-	struct book * next;
-	int book_num;
-	char *book_name;
-	char *publish;
+typedef struct Book {
+	struct Book *nxt;
+	char isLend;
 	char *author;
-	long isbn;
+	char *bookName;
 	char *location;
-	char is_lend;
-};
+	char *publish;
+	long isbn;
+	int bookNum;
+} Book;
+
+void PrintBookList(Book *head) {
+	Book *cur = head;
+
+	while (cur != NULL) {
+		// TODO : add printf to print contents
+		printf("* %s\n", cur->author);
+		cur = cur->nxt;
+	}
+}
+
+void AddBook(Book *head, Book book) {
+	Book *cur = head;
+
+	while (cur->nxt != NULL) {
+		cur = cur->nxt;
+	}
+
+	cur->nxt = malloc(sizeof(Book));
+
+	cur = cur->nxt;
+	
+	cur->isLend = book.isLend;
+	cur->author = book.author;
+	cur->bookName = book.bookName;
+	cur->location = book.location;
+	cur->publish = book.publish;
+	cur->isbn = book.isbn;
+	cur->bookNum = book.bookNum;
+
+	cur->nxt = NULL;
+}
 
