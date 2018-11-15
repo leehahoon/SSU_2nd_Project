f2acf8e1de778d1e8f2fcc9c42b0ebad4bf9ff61
Wookje Kwon <wookje.happy@gmail.com>
2018-11-15 16:37:47 +0900

    book file added

--- book.h
+++ book.h
@@ -2,44 +2,19 @@
 
 typedef struct Book {
 	struct Book *nxt;
-	char isLend;
+	char lendAble;
 	char *author;
 	char *bookName;
 	char *location;
 	char *publish;
-	long isbn;
+	char *isbn;
 	int bookNum;
 } Book;
 
-void PrintBookList(Book *head) {
-	Book *cur = head;
-
-	while (cur != NULL) {
-		// TODO : add printf to print contents
-		printf("* %s\n", cur->author);
-		cur = cur->nxt;
-	}
-}
-
-void AddBook(Book *head, Book book) {
-	Book *cur = head;
-
-	while (cur->nxt != NULL) {
-		cur = cur->nxt;
-	}
-
-	cur->nxt = malloc(sizeof(Book));
-
-	cur = cur->nxt;
-	
-	cur->isLend = book.isLend;
-	cur->author = book.author;
-	cur->bookName = book.bookName;
-	cur->location = book.location;
-	cur->publish = book.publish;
-	cur->isbn = book.isbn;
-	cur->bookNum = book.bookNum;
-
-	cur->nxt = NULL;
-}
-
+void PrintBookInfo(Book *cur);
+int IsSubstring(char *A, char *B); // KMP algorithm written by Wookje Kwon
+void SearchBookName(Book *head, char *target, int searchType);
+void AddBook(Book *head, Book book);
+void RemoveBook(Book *head, int bookNum);
+void lendBook(Book *head, int bookNum);
+void returnBook(Book *head, int bookNum);
