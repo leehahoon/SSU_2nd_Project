d6867a389f2469526dc91d7e4d599dd5753fd7d4
Wookje Kwon <wookje.happy@gmail.com>
2018-12-07 16:31:14 +0900

    Update book.c

--- book.c
+++ book.c
@@ -92,6 +92,12 @@ int SearchBook(Book *head, char *target, long long targetIsbn, int searchType, i
         }
     }
 
+    if (ret == 0 && isPrint == 1) {
+        printf("\n-----------------------------\n");
+        printf("       찾는 책이 없어요!!!");
+        printf("-----------------------------\n");    
+    }
+
     return ret;
 }
 
